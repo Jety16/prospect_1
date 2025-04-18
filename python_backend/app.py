c37841cabe9cabad276a5f0cbb69ca7d72050638
juan-dev <juan-dev@juan-dev.juan-dev>
2025-04-18 11:44:27 -0300

    fix connection to db

--- python_backend/app.py
+++ python_backend/app.py
@@ -21,15 +21,14 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-db_host = os.getenv('DB_HOST')           # 'cloudsql-proxy'
-db_port = os.getenv('DB_PORT', '5432')   # '5432'
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT', '5432')
 db_name = os.getenv('DB_NAME')
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f'postgresql+psycopg2://{db_user}:{db_password}@'
-    f'{db_host}:{db_port}/{db_name}'
+    f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 )
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
