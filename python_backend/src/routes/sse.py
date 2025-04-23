fa5095bd5d6368a837cd85139b37e9d66f502ded
juan-dev <juan-dev@juan-dev.juan-dev>
2025-04-23 15:33:21 -0300

    Revert "fix circular import"
    This reverts commit 8f322b4cb1613944208d5271b5fe86b371ff2608.

--- python_backend/src/routes/sse.py
+++ python_backend/src/routes/sse.py
@@ -3,6 +3,7 @@ import time
 import logging
 from flask import Blueprint, Response, stream_with_context
 from src.models import File
+from src import db
 
 sse_blueprint = Blueprint('sse', __name__)
 logger = logging.getLogger(__name__)
